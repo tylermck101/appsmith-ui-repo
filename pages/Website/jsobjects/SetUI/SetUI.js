80807324c334e528df43e3f3b0c43e7d56af427b
ashlynn braisted <braisted.a@northeastern.edu>
2024-04-19 07:21:44 +0000

    colors

--- pages/Website/jsobjects/SetUI/SetUI.js
+++ pages/Website/jsobjects/SetUI/SetUI.js
@@ -1,8 +1,8 @@
-export default function applyColorScheme(colors) {
-	// Assuming `colors` is the API response containing the color scheme
-	appsmith.store.update({
-		headerColor: colors.headerColor,
-		textColor: colors.textColor,
-		backgroundColor: colors.backgroundColor
-	});
+export default {
+    async setColor() {
+				let colors = await ColorSchemeByMood.run(UserMood.data);
+        storeValue('primaryColor', colors.headerColor);
+        storeValue('backgroundColor', colors.backgroundColor);
+			  storeValue('textColor', colors.textColor);
+    }
 }
--- pages/Welcome/jsobjects/SetUI/SetUI.js
+++
@@ -1,8 +0,0 @@
-export default function applyColorScheme(colors) {
-	// Assuming `colors` is the API response containing the color scheme
-	appsmith.store.update({
-		headerColor: colors.headerColor,
-		textColor: colors.textColor,
-		backgroundColor: colors.backgroundColor
-	});
-}
